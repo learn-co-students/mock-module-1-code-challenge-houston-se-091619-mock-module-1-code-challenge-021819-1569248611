56a372487be64188c123bc55f5d6462b7a0a12aa
github username <github email address>
2019-09-23 10:51:49 -0500

    Mock test

--- author.rb
+++ author.rb
@@ -1,1 +1,18 @@
+class Author
 
+    attr_accessor :authors
+
+    @@all = []
+
+
+    def initialize (authors)
+        @authors = authors
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,36 @@
+class Book
 
+    attr_accessor :books, :author, :title, :word_count, :most_words
+
+    @@all = []
+    @@aa = []
+
+
+    def initialize (books, title, word_count)
+        @books = books
+        @author = author
+        @@all << self
+        @title = title
+        @word_count = word_count
+
+    end
+
+    def self.all
+        @@all
+    end
+
+    def title
+        @title
+    end
+
+    def word_count
+    @word_count
+    end
+
+    def most_words
+        @@aa << self.all most_words
+    end
+
+    
+
+end
