e97d620a32104502976c471f962b6d99167dcdd4
Your Name <you@example.com>
2019-09-23 10:50:37 -0500

    done.

--- author.rb
+++ author.rb
@@ -1,1 +1,25 @@
+class Author 
 
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name 
+        @@all << self
+    end
+
+    def self.all 
+        @@all 
+    end
+
+    def books 
+        Book.all.select {|book| book.author == self.name}
+    end
+
+    def write_book(title, word_count)
+        Book.new(title, word_count, self)
+    end
+
+
+end
--- book_author.rb
+++ book_author.rb
@@ -1,1 +1,20 @@
+class BookAuthor 
 
+    attr_accessor :book, :author 
+
+    @@all = []
+
+    def initialize(book, author)
+        @book = book
+        @author = author 
+        @@all << self 
+    end
+
+    def self.all
+        @@all 
+    end
+
+
+
+
+end
