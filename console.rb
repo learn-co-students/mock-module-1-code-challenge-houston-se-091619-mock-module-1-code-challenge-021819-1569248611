0965306f6144d9a607c342a8e05a9cc65d58d44d
alexiscash <alexiscash97@rocketmail.com>
2019-09-23 10:47:44 -0500

    ayy lmao

--- console.rb
+++ console.rb
@@ -3,4 +3,13 @@ require_relative './book'
 require_relative './author'
 require_relative './book_author.rb'
 
+# book = Book.new('Harry Potter', 40)
+# author = Author.new('JK Rowling')
+# a2 = Author.new('Clarke')
+# b2 = Book.new('Robotics', 100)
+# harry = BookAuthor.new(book, author)
+# h2 = BookAuthor.new(b2, a2)
+# b3 = Book.new('something', 1000)
+# connect = BookAuthor.new(b3, author)
+
 binding.pry
