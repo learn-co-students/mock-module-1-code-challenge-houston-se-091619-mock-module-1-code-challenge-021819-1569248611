2a0cdca2623b54e3804b1b6d0323c82e865c385d
github username <github email address>
2019-09-23 10:51:21 -0500

    my_code_chal

--- console.rb
+++ console.rb
@@ -3,4 +3,14 @@ require_relative './book'
 require_relative './author'
 require_relative './book_author.rb'
 
+b1 = Book.new("The Hat", 150, "John D.")
+b2 = Book.new("The Shirt", 200, "Jane D.")
+b3 = Book.new("The Pants", 250, "Joe D.")
+
+a1 = Author.new("Penny P.")
+a2 = Author.new("Dime D.")
+
+a1.write_book("The Cap", 300)
+
 binding.pry
+0
